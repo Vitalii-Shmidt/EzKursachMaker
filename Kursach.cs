ef51314946c03f714aab613b3462c4979c5b4943
binary robot <vitalii.revival@gmail.com>
2021-06-01 13:15:18 +0300

    Core changes. Add all needed features.

---
+++ Kursach.cs
@@ -0,0 +1,8 @@
+namespace EzKursachMaker
+{
+    public class Курсач
+    {
+        public static Курсач Сделать() 
+            => new Курсач();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EzKursachMaker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Курсач.Сделать();   
+        }
+    }
+}
